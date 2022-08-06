804abe0d9064c97bfb66b80ff3de4c95a54f7661
jimmychim59 <61173651+jimmychim59@users.noreply.github.com>
2022-08-06 12:03:09 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -16,6 +16,8 @@ function CarOmni () {
 function Joystick () {
     x = Math.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, -255, 255)
     y = Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, -255, 255)
+    myDot.set(LedSpriteProperty.X, Math.map(x, -255, 255, 0, Display))
+    myDot.set(LedSpriteProperty.X, Math.map(y, -255, 255, Display, 0))
     dead_zone()
 }
 input.onButtonPressed(Button.B, function () {
@@ -66,43 +68,37 @@ function CarDrift () {
         radio.sendValue("a2", 0)
     }
 }
+let sflag = 0
 let w2 = 0
 let w1 = 0
 let y = 0
 let x = 0
 let moveFlag = 0
-let sflag = 0
+let Display = 0
+let myDot: game.LedSprite = null
 led.setBrightness(255)
 basic.showIcon(IconNames.Happy)
 let radio_gp = 39
 radio.setGroup(radio_gp)
-let myDot = game.createSprite(3, 3)
+myDot = game.createSprite(3, 3)
 pins.setPull(DigitalPin.P13, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P14, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P15, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P16, PinPullMode.PullNone)
+Display = 4
 music.setBuiltInSpeakerEnabled(true)
-let Maxspeed = 10
-let Display = 4
-sflag = -1
 moveFlag = -1
 music.setVolume(80)
 music.stopMelody(MelodyStopOptions.All)
 basic.forever(function () {
-    // p15 Red Button = Stop (moveFlag = -1)
-    // 
-    // p13 Green Button = Go (moveFlag = 1)
-    // p14 Yellow Button = Turn (moveFlag = 2)
-    // p16 Blue Button = Drift (moveFlag = 3)
     if (pins.digitalReadPin(DigitalPin.P15) == 0) {
         moveFlag = -1
         basic.showIcon(IconNames.No)
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 500, 500, 255, 0, 50, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
         radio.sendValue("stop", -1)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
     } else if (pins.digitalReadPin(DigitalPin.P13) == 0) {
         moveFlag = 1
         basic.showIcon(IconNames.Happy)
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 500, 499, 255, 0, 750, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
     } else if (pins.digitalReadPin(DigitalPin.P14) == 0) {
         moveFlag = 2
         basic.showLeds(`
@@ -112,7 +108,6 @@ basic.forever(function () {
             . # . # .
             . . . . .
             `)
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 400, 600, 255, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.InBackground)
     } else if (pins.digitalReadPin(DigitalPin.P16) == 0) {
         moveFlag = 3
         basic.showLeds(`
@@ -122,7 +117,6 @@ basic.forever(function () {
             . . # . .
             . # # # .
             `)
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 1600, 1, 255, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
     } else if (moveFlag > 0) {
         CarMove()
     }
