ddd913a58849dbe74a15c55f9edf3c87395236ae
jimmychim59 <61173651+jimmychim59@users.noreply.github.com>
2022-08-07 14:59:21 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 // 降低靈敏度
 function dead_zone () {
-    if (x > -140 && x < 140) {
+    if (x < joystick_deadzone && x > joystick_deadzone * -1) {
         x = 0
     }
-    if (y > -140 && y < 140) {
+    if (y < joystick_deadzone && y > joystick_deadzone * -1) {
         y = 0
     }
 }
@@ -18,10 +18,10 @@ input.onButtonPressed(Button.A, function () {
 })
 function Joystick () {
     x = Math.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, -255, 255)
-    y = Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, -255, 255)
-    myDot.set(LedSpriteProperty.X, Math.map(x, -255, 255, 0, Display))
-    myDot.set(LedSpriteProperty.Y, Math.map(y, -255, 255, Display, 0))
+    y = Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 255, -255)
     dead_zone()
+    myDot.set(LedSpriteProperty.X, Math.map(x, -255, 255, 0, Display))
+    myDot.set(LedSpriteProperty.Y, Math.map(y, -255, 255, 0, Display))
 }
 input.onButtonPressed(Button.B, function () {
     radio.sendValue("STOP", 0)
@@ -63,9 +63,9 @@ function CarDrift () {
     radio.sendValue("a1", x)
     if (y > 0) {
         if (x > 0) {
-            radio.sendValue("a2", Math.map(y, 0, 255, 0, -100))
+            radio.sendValue("a2", Math.map(y, -255, 255, 0, -150))
         } else {
-            radio.sendValue("a2", Math.map(y, 0, 255, 0, 100))
+            radio.sendValue("a2", Math.map(y, -255, 255, 0, 150))
         }
     } else {
         radio.sendValue("a2", 0)
@@ -77,6 +77,7 @@ let w1 = 0
 let y = 0
 let x = 0
 let moveFlag = 0
+let joystick_deadzone = 0
 let Display = 0
 let myDot: game.LedSprite = null
 led.setBrightness(255)
@@ -88,7 +89,8 @@ pins.setPull(DigitalPin.P13, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P14, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P15, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P16, PinPullMode.PullNone)
-Display = 4
+Display = 5
+joystick_deadzone = 140
 music.setBuiltInSpeakerEnabled(true)
 moveFlag = -1
 music.setVolume(80)
@@ -96,14 +98,21 @@ music.stopMelody(MelodyStopOptions.All)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P15) == 0) {
         moveFlag = -1
+        led.setBrightness(50)
         basic.showIcon(IconNames.No)
         radio.sendValue("stop", -1)
         music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
+        basic.pause(100)
     } else if (pins.digitalReadPin(DigitalPin.P13) == 0) {
         moveFlag = 1
+        led.setBrightness(255)
         basic.showIcon(IconNames.Happy)
+        radio.sendValue("mode", moveFlag)
+        music.playTone(523, music.beat(BeatFraction.Half))
+        basic.pause(100)
     } else if (pins.digitalReadPin(DigitalPin.P14) == 0) {
         moveFlag = 2
+        led.setBrightness(255)
         basic.showLeds(`
             . . . . .
             . # . # .
@@ -111,8 +120,12 @@ basic.forever(function () {
             . # . # .
             . . . . .
             `)
+        radio.sendValue("mode", moveFlag)
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        basic.pause(100)
     } else if (pins.digitalReadPin(DigitalPin.P16) == 0) {
         moveFlag = 3
+        led.setBrightness(255)
         basic.showLeds(`
             # # . # #
             # . . . #
@@ -120,7 +133,10 @@ basic.forever(function () {
             . . # . .
             . # # # .
             `)
-    } else if (moveFlag > 0) {
+        radio.sendValue("mode", moveFlag)
+        music.playTone(131, music.beat(BeatFraction.Quarter))
+        basic.pause(100)
+    } else {
         CarMove()
     }
 })
